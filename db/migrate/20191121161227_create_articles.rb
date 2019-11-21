386d604dc75893762df161b6a9bb1a13e56153a6
he jin <hejin82@gmail.com>
2019-11-21 16:17:14 +0000

    commit

--- db/migrate/20191121161227_create_articles.rb
+++ db/migrate/20191121161227_create_articles.rb
@@ -3,7 +3,7 @@ class CreateArticles < ActiveRecord::Migration[6.0]
     create_table :articles do |t|
       t.string :title
       t.string :text
-      t.array :tags
+      t.string :tags
 
       t.timestamps
     end
