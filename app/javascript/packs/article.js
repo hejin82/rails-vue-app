1be460a1862d32b83a7a52cf21baf86a4da6167c
he jin <hejin82@gmail.com>
2019-11-21 16:45:31 +0000

    commit

--- app/javascript/packs/article.js
+++ app/javascript/packs/article.js
@@ -6,15 +6,22 @@ document.addEventListener('DOMContentLoaded', () => {
     el: '#app',
     data: {
       message: "Can you say hello?",
-      info: null
+      info: null,
+      loading: true,
+      errored: false
     },
     components: {
 
     },
     mounted () {
-    axios
-      .get('/articles.json')
-      .then(response => (this.info = response.data))
+      axios
+        .get('/articles.json')
+        .then(response => (this.info = response.data))
+        .catch(error => {
+          console.log(error)
+          this.errored = true
+        })
+        .finally(() => this.loading = false)
     }
   })
 })
