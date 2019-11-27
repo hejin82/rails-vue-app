84359ceb94e6a3e5beb245399b13a8c0cade2004
hejin <hejin82@gmail.com>
2019-11-27 20:42:06 +0900

    commit

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -3,17 +3,36 @@
     <AppHeader/>
     <div class="container-fluid">
         <button class="btn btn-primary">button</button>
-        <AppTree :data="treeData"/>
+        <div class="container">
+            <AppTree :data="treeData"/>
+        </div>
+        test checkbox:
+        <form role="form">
+          <div class="form-check abc-checkbox">
+            <input class="form-check-input" id="checkbox1" type="checkbox">
+            <label class="form-check-label" for="checkbox1">
+              Check me out
+            </label>
+          </div>
+        </form>
         <app-footer></app-footer>
     </div>
   </div>
 </template>
 
 <script>
-import AppHeader from './src/components/AppHeader.vue'
+import AppHeader from './src/components/app-header.vue'
 import AppFooter from './src/components/app-footer.vue'
 
 export default {
+  data: {
+
+  },
+  methods: {
+    treeData() {
+
+    }
+  },
   components: {
     AppHeader,
     AppFooter
