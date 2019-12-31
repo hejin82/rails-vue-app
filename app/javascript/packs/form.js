1dafca638849ec7249915b4b8ce6863a5f065249
hejin <hejin82@gmail.com>
2020-01-01 01:28:07 +0900

    add veux

--- app/javascript/packs/form.js
+++ app/javascript/packs/form.js
@@ -2,12 +2,17 @@ import Vue from 'vue';
 import App from '../components/form.vue';
 import VueMask from 'v-mask';
 import Vuelidate from 'vuelidate';
+import Vuex from 'vuex';
 
 Vue.use(VueMask);
 Vue.use(Vuelidate);
+Vue.use(Vuex);
+
+const stroe = new Vuex.Store({});
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
+    store: stroe,
     render: h => h(App)
   }).$mount();
   document.body.appendChild(app.$el);
