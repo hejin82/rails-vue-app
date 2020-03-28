8dd5ad165e068bf7aeab17327e8202cce9683238
hejin <hejin82@gmail.com>
2020-03-29 00:25:40 +0900

    commit

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,26 +1,8 @@
 <template>
   <div id="app">
     <AppHeader/>
-    <div class="container-fluid">
-        <button class="btn btn-primary">button</button>
-        <div class="container">
-            <AppTree :data="treeData"/>
-        </div>
-        test checkbox:
-        <form role="form">
-          <div class="form-check abc-checkbox">
-            <input class="form-check-input" id="checkbox1" type="checkbox">
-            <label class="form-check-label" for="checkbox1">
-              Check me out
-            </label>
-          </div>
-        </form>
-        <app-footer></app-footer>
-        <app-button @click="buttonClick">button</app-button>
-    </div>
-	  <div class="container">
-		  <app-login/>
-	  </div>
+    <app-footer></app-footer>
+    <app-button @click="buttonClick">button</app-button>
   </div>
 </template>
 
